9a76d09ca4cffa2e7c9d336abad2e4b83f3ccde5
APSanFilippo <60323681+APSanFilippo@users.noreply.github.com>
2020-06-13 00:18:39 -0500

    Update app.py

--- Webpage/app.py
+++ Webpage/app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/embed")
-def embed():
-    return render_template("embed.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
